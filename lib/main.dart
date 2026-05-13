744f62ca5aae90087265d34e46f9efcd4c9a94c3
fabriziovenegas <fabrizio.adrian.venegas@gmail.com>
2026-05-12 23:05:30 -0500

    Practicando 1.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:whatsappg15/examples/tabbar_manual_page.dart';
+import 'package:whatsappg15/examples/tabbar_page.dart';
 import 'package:whatsappg15/pages/home_page.dart';
 import 'package:whatsappg15/tabs/call_tab.dart';
 
@@ -8,7 +10,7 @@ void main() {
   runApp(
     MaterialApp(
       theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
-      home: HomePage(),
+      home: TabbarManualPage(),
       debugShowCheckedModeBanner: false,
     ),
   );
